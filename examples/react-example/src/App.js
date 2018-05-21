57d0e74579a346e6ff716f641f20888281178042
Andrew Winget (Standard) <scipioaffricanus@gmail.com>
2018-05-21 15:17:26 -0400

    connect the rendering to the redux core state in react example

--- examples/react-example/src/App.js
+++ examples/react-example/src/App.js
@@ -1,30 +1,73 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 import m3core from 'minimal_redux_poc';
 
 class App extends Component {
-  showManifest(event) {
-    event.preventDefault();
-    console.log(m3core.store.getState());
-  }
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <input id="manifestURL" type="text"/>
-        <button id="fetchBtn" type="submit" onClick={this.showManifest}>Fetch Manifest</button>
-        <pre id="exampleManifest"></pre>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            formValue: '',
+            lastRequested: '',
+            content: 'Nothing Selected Yet'
+        };
+        this.fetchManifest = this.fetchManifest.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
+
+        m3core.store.subscribe(()=>{
+            let manifest = m3core.store.getState().manifests[this.state.lastRequested];
+
+            if (manifest) {
+                if (manifest.isFetching) {
+                    this.setState({
+                        content: '☕'
+                    });
+                    return;
+                } else if (manifest.error) {
+                    this.setState({
+                        content: manifest.error.message
+                    });
+                    return;
+                }
+
+                this.setState({
+                    content: JSON.stringify(manifest.json, 0, 2)
+                });
+            }
+        });
+    }
+    fetchManifest(event) {
+        event.preventDefault();
+        console.log(this.state.formValue);
+        let f = m3core.actions.fetchManifest(this.state.formValue);
+        this.setState({
+            lastRequested: this.state.formValue
+        });
+        m3core.store.dispatch(f);
+
+    }
+    handleInputChange(event) {
+        let _this = this;
+        event.preventDefault();
+        _this.setState({
+            formValue: event.target.value
+        });
+    }
+    render() {
+        // let manifest = m3core.store.getState().manifests[Object.keys(m3core.store.getState().manifests)[0]];
+
+        return (
+                <div className="App">
+                <form onSubmit={this.fetchManifest}>
+                <input value={this.state.formValue} id="manifestURL" type="text" onChange={this.handleInputChange}/>
+
+                <button id="fetchBtn" type="submit">Fetch Manifest</button>
+                <pre id="exampleManifest">
+                { this.state.content }
+            </pre>
+                </form>
+                </div>
+        );
+    }
 }
 
 export default App;
