bf033997d92799329409a24314384331233268c3
Mark Allen Matney, Jr <mmatney@library.ucla.edu>
2018-05-21 13:56:07 -0400

    import redux store into React example

--- examples/react-example/src/App.js
+++ examples/react-example/src/App.js
@@ -2,7 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import m3core from 'minimal_redux_poc';
+
 class App extends Component {
+  showManifest(event) {
+    event.preventDefault();
+    console.log(m3core.store.getState());
+  }
   render() {
     return (
       <div className="App">
@@ -13,6 +19,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <input id="manifestURL" type="text"/>
+        <button id="fetchBtn" type="submit" onClick={this.showManifest}>Fetch Manifest</button>
+        <pre id="exampleManifest"></pre>
       </div>
     );
   }
