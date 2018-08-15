37a5061ba2a77989c7764abab1ab539099d547f1
aeschylus <scipioaffricanus@gmail.com>
2018-08-15 11:24:07 -0700

    fix style errors in config

--- minimal_redux_poc/integrations.config.js
+++ minimal_redux_poc/integrations.config.js
@@ -18,10 +18,10 @@ module.exports = [
         {
           test: /\.scss$/,
           use: [
-            "style-loader", // creates style nodes from JS strings
-            "css-loader", // translates CSS into CommonJS
-            "sass-loader" // compiles Sass to CSS, using Node Sass by default
-          ]
+            'style-loader', // creates style nodes from JS strings
+            'css-loader', // translates CSS into CommonJS
+            'sass-loader', // compiles Sass to CSS, using Node Sass by default
+          ],
         }],
     },
   },
