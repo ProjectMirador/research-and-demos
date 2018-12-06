f47247360f24fe0f7b28e750260c3a5753fb6a30
Jack Reed <phillipjreed@gmail.com>
2018-12-06 10:56:52 +0000

    implement a configuration pattern with update option

--- minimal_redux_poc/src/actions/index.js
+++ minimal_redux_poc/src/actions/index.js
@@ -6,6 +6,27 @@ import ManifestFetcher from '../lib/ManifestFetcher';
  * @namespace ActionCreators
  */
 
+
+/**
+ * setConfig - action creator
+ *
+ * @param  {Object} config
+* @memberof ActionCreators
+ */
+export function setConfig(config) {
+  return { type: ActionTypes.SET_CONFIG, config };
+}
+
+/**
+ * updateConfig - action creator
+ *
+ * @param  {Object} config
+* @memberof ActionCreators
+ */
+export function updateConfig(config) {
+  return { type: ActionTypes.UPDATE_CONFIG, config };
+}
+
 /**
  * focusWindow - action creator
  *
