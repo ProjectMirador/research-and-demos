ee0cfed9a58bee39070354aa0a5b98bfd3d7b6c5
Jack Reed <phillipjreed@gmail.com>
2018-12-20 15:44:47 -0700

    update Mirador api to return an instance based object, capable of registering plugins

--- minimal_redux_poc/src/index.js
+++ minimal_redux_poc/src/index.js
@@ -8,19 +8,52 @@ import settings from './config/settings';
 import './styles/index.scss';
 
 /**
- * Default Mirador instantiation
+ * Default MiradorViewer instantiation
  */
-export default function Mirador(config) {
-  const action = actions.setConfig(deepmerge(settings, config));
-  store.dispatch(action);
-  ReactDOM.render(
-    <Provider store={store}>
-      <App config={config} />
-    </Provider>,
-    document.getElementById(config.id),
-  );
-  return {
-    actions,
-    store,
-  };
+class MiradorViewer {
+  /**
+   * constructor
+   *
+   * @param  {Object} config
+   * @return {Object}
+   */
+  constructor(config) {
+    const action = actions.setConfig(deepmerge(settings, config));
+    store.dispatch(action);
+    this.actions = actions;
+    this.store = store;
+    this.registerPlugins();
+    ReactDOM.render(
+      <Provider store={store}>
+        <App config={config} />
+      </Provider>,
+      document.getElementById(config.id),
+    );
+  }
+
+  /**
+   * registerPlugins
+   */
+  registerPlugins() {
+    const { config } = this.store.getState();
+    const { plugins } = config;
+    const that = this;
+    plugins.forEach((plugin) => {
+      if (!window[plugin]) {
+        console.log(`${plugin} plugin is not available`);
+        return;
+      }
+      // Should every MiradorViewer component do something similar?
+      window[plugin].components.forEach((component) => {
+        if (component.parent === 'global') component.init(that);
+      });
+    });
+  }
 }
+
+const exports = {
+  // Other components/classes can be exported here for people who want to customize
+  MiradorViewer,
+};
+
+export default exports;
