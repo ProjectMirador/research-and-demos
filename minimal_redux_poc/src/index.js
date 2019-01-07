90fcd045e981cb72e56c1bd1ac9f3b869ba2496a
Andrew Winget (Standard) <scipioaffricanus@gmail.com>
2019-01-07 15:09:10 -0800

    separate library and instance API layers into separate files

--- minimal_redux_poc/src/index.js
+++ minimal_redux_poc/src/index.js
@@ -1,39 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import deepmerge from 'deepmerge';
-import App from './components/App';
-import { actions, store } from './store';
-import settings from './config/settings';
-import './styles/index.scss';
+import init from './init';
 
-/**
- * Default Mirador instantiation
- */
-const Mirador = function(config) {
-  const viewer = {
-    actions,
-    store,
-  };
-  const action = actions.setConfig(deepmerge(settings, config));
-  store.dispatch(action);
-
-  Object.keys(Mirador.plugins).forEach((plugin)=>{
-    Mirador.plugins[plugin].components.forEach((component)=>{
-      if (component.parent === 'global') component.init(viewer);
-    });
-  });
-
-  ReactDOM.render(
-    <Provider store={store}>
-      <App config={config} />
-    </Provider>,
-    document.getElementById(config.id),
-  );
-
-  return viewer;
+const exports = {
+  viewer: init,
+  plugins: []
 }
 
-Mirador.plugins = {};
-
-export default Mirador
+export default exports;
